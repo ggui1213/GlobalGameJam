03db7993bcac83020864de0f142b1426947e8975
BennetYrWang <BennetYrWang@users.noreply.github.com>
2026-01-31 15:57:59 -0500

    Player In Game Behavior

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -6,17 +6,40 @@ namespace Script
     public class Player : MonoBehaviour
     {
         private InputSystem_Actions _inputSystemActions;
+        private Rigidbody2D _rigidbody2D;
+        [SerializeField] private float gravityScale = 1;
+        [SerializeField] private PlayerGravityState initialGravityState = PlayerGravityState.GravityOn;
+        private PlayerGravityState _currentGravityState;
 
 
         private void Start()
         {
             _inputSystemActions = GameManager.Instance.InputActions;
+            _rigidbody2D = GetComponent<Rigidbody2D>();
+            
+            _currentGravityState = initialGravityState;
+            _rigidbody2D.gravityScale = initialGravityState == PlayerGravityState.GravityOn ? gravityScale : 0;
         }
 
         private void Update()
         {
-            
+            if (_inputSystemActions.Player.ToggleGravity.triggered)
+            {
+                _currentGravityState = _currentGravityState == PlayerGravityState.GravityOn
+                    ? PlayerGravityState.GravityOff
+                    : PlayerGravityState.GravityOn;
+                
+                _rigidbody2D.gravityScale = _currentGravityState == PlayerGravityState.GravityOn ? gravityScale : 0;
+            }
         }
+
+        private void OnEnterTrap()
+        {
+            throw new NotImplementedException();
+        }
+
+        
+        
 
 
         /// <summary>
@@ -28,10 +51,11 @@ namespace Script
             
             OnEnterTrap();
         }
-
-        private void OnEnterTrap()
+        
+        public enum PlayerGravityState
         {
-            
+            GravityOn,
+            GravityOff
         }
     }
 }
